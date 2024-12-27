e0c92fdfb77c85f8d7cc852ee470f5cbcb3b13df
MarianoHernandezEscobal <mh202428@fi365.ort.edu.uy>
2024-12-27 14:09:41 -0300

    changes

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -8,12 +8,11 @@ export class MailService {
   async sendEmail(to: string, subject: string, content: string): Promise<void> {
     try {
       await this.mailerService.sendMail({
-        to, // Dirección del destinatario
-        subject, // Asunto del correo
-        text: content, // Contenido en texto plano
-        html: `<p>${content}</p>`, // Contenido en formato HTML
+        to,
+        subject,
+        text: content,
+        html: `<p>${content}</p>`,
       });
-      console.log('Correo enviado con éxito');
     } catch (error) {
       console.error('Error al enviar el correo:', error);
       throw new Error('No se pudo enviar el correo');
