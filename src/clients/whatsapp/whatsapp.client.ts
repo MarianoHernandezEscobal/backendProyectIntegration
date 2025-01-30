5744a30e2be78273fd2d720adc0cf6a7205381f4
MarianoHernandezEscobal <mh202428@fi365.ort.edu.uy>
2025-01-30 17:05:17 -0300

    changes

--- src/clients/whatsapp/whatsapp.client.ts
+++ src/clients/whatsapp/whatsapp.client.ts
@@ -18,7 +18,6 @@ export class WhatsAppClient implements OnModuleInit {
       this.client = new Client({
         authStrategy: new LocalAuth(), // Guarda la sesión en el servidor
         puppeteer: {
-          executablePath: '/usr/bin/chromium-browser',
           headless: true, // Modo sin interfaz gráfica
           args: [
             '--no-sandbox',
@@ -27,12 +26,7 @@ export class WhatsAppClient implements OnModuleInit {
             '--disable-dev-shm-usage',
             '--disable-software-rasterizer',
             '--disable-extensions',
-            '--disable-background-timer-throttling',
-            '--disable-backgrounding-occluded-windows',
-            '--disable-renderer-backgrounding',
-            '--disable-accelerated-2d-canvas',
             '--single-process',
-            '--no-zygote'
           ],
         }
       });
